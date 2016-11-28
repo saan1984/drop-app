6d13827a3e25e38c9f48250a227498ee50b34c88
spatel10 <sandeepkumar_patel@intuit.com>
2016-11-28 08:58:58 +0530

    changes

--- pdfServer.js
+++ pdfServer.js
@@ -1,32 +1,31 @@
 let fs = require('fs'),
     PDFParser = require("pdf2json");
 
-let pdfParser = new PDFParser();
 
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-pdfParser.on("pdfParser_dataReady", pdfData => {
-    fs.writeFile("invoice.json", JSON.stringify(pdfData));
-});
-//pdfParser.loadPDF("invoice.pdf");
 
 var express = require('express');
 var bodyParser = require('body-parser')
 
 var app = express();
 app.use(express.static('.'));
-app.use( bodyParser.json({limit: '1mb'}) );
+app.use( bodyParser.json({limit: '10mb'}) );
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/pdf', function(req, res){
-    console.log( req.body.data);
-
+    console.log("aaa", req.body.data);
     var pdf = req.body.data;
+    var name = req.body.name;
+    var pdf = pdf.replace('data:application/pdf;base64,', '');
 
-   var pdf = pdf.replace('data:application/pdf;base64,', '');
-
-    res.send('received');
-   /* fs.writeFile(name, pdf, 'base64', function(err) {
+   fs.writeFile(name, pdf, 'base64', function(err) {
         console.log(err);
-    });*/
+       let pdfParser = new PDFParser();
+       pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
+       pdfParser.on("pdfParser_dataReady", pdfData => {
+           res.send(JSON.stringify(pdfData));
+       });
+        pdfParser.loadPDF(name);
+    });
 });
 
 var server = app.listen(3000, function() {
